3a5ac798840ae6bb116d1178305b73224d2a769c
Vlad Pantyukhin <pantyukhinvlad@gmail.com>
2023-07-08 09:41:00 +0300

    Исправил задачу 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -2,7 +2,7 @@ function isEmpty(obj) {
   // ваш код...
   if (typeof obj === "object") {
     for (let key in obj) {
-      return !(obj[key] || obj[key] === undefined);
+      return false;
     }
     return true;
   }
