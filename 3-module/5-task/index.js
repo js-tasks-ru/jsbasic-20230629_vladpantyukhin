97e1b0afd11146b4a69caae7e5719f3708214ea5
Vlad Pantyukhin <pantyukhinvlad@gmail.com>
2023-07-11 18:42:01 +0300

    Задачи 3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
   // ваш код...
+  const numArr = str
+                  .split(' ')
+                  .filter(item => !isNaN(parseFloat(item)))
+                  .map(item => parseFloat(item));
+
+  const result = {
+    min: Math.min(...numArr),
+    max: Math.max(...numArr)
+  };
+
+  return result;
 }
